d465f7534e0f7414552306a9ad675c8b6837cee6
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2023-11-23 14:24:39 +0900

    add error test

--- concurrency.go
+++ concurrency.go
@@ -1,23 +1,42 @@
 package isunippets
 
 import (
+	"context"
 	"fmt"
 	"github.com/sourcegraph/conc/pool"
 )
 
-func RunConcurrent[T any](requests []*T, concurrency int, execute func(*T, int) *T) error {
+type RunConcurrentOptions struct {
+	Concurrency   int
+	CancelOnError bool
+}
+
+func RunConcurrent[T any](requests []*T, options *RunConcurrentOptions, execute func(context.Context, *T, int) (*T, error)) error {
 	fmt.Printf("requests: %v\n", requests)
-	p := pool.NewWithResults[*T]().WithMaxGoroutines(concurrency)
+	ctx := context.Background()
+	p := pool.NewWithResults[*T]()
+	if options != nil {
+		if options.Concurrency > 0 {
+			p = p.WithMaxGoroutines(options.Concurrency)
+		}
+	}
+
+	wp := p.WithContext(ctx).WithCollectErrored()
+	if options != nil {
+		if options.CancelOnError {
+			wp = wp.WithCancelOnError()
+		}
+	}
+
 	for i, r := range requests {
 		i := i
 		r := r
 		fmt.Printf("i: %d, %p\n", i, r)
-		p.Go(func() *T {
-			return execute(r, i)
+		wp.Go(func(c context.Context) (*T, error) {
+			return execute(c, r, i)
 		})
 	}
-	res := p.Wait()
+	res, err := wp.Wait()
 	fmt.Printf("res: %v\n", res)
-
-	return nil
+	return err
 }
--- concurrency_test.go
+++ concurrency_test.go
@@ -1,6 +1,9 @@
 package isunippets
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"sort"
 	"testing"
@@ -23,18 +26,76 @@ func TestRunConcurrent(t *testing.T) {
 		{params: 6},
 	}
 
-	start := time.Now()
-	err := RunConcurrent(requests, 2, func(r *MyConcurrencyRequest, i int) *MyConcurrencyRequest {
-		time.Sleep(1 * time.Second)
+	opts := &RunConcurrentOptions{
+		Concurrency: 2,
+	}
 
+	start := time.Now()
+	err := RunConcurrent(requests, opts, func(c context.Context, r *MyConcurrencyRequest, i int) (*MyConcurrencyRequest, error) {
 		r.index = i
 		r.result = r.params + i
-		return r
+
+		time.Sleep(1 * time.Second)
+
+		return r, nil
 	})
 	assert.NoError(err)
 
 	elapsed := time.Since(start)
 	assert.LessOrEqual(elapsed.Seconds(), 2.5)
+
+	sort.Slice(requests, func(i, j int) bool {
+		return requests[i].index < requests[j].index
+	})
+
+	expected := []*MyConcurrencyRequest{
+		{index: 0, params: 0, result: 0},
+		{index: 1, params: 2, result: 3},
+		{index: 2, params: 4, result: 6},
+		{index: 3, params: 6, result: 9},
+	}
+
+	assert.Equal(expected, requests)
+}
+
+func TestRunConcurrent_WithError(t *testing.T) {
+	assert := assert.New(t)
+
+	type MyConcurrencyRequest struct {
+		index  int
+		params int
+		result int
+	}
+
+	requests := []*MyConcurrencyRequest{
+		{params: 0},
+		{params: 2},
+		{params: 4},
+		{params: 6},
+	}
+
+	opts := &RunConcurrentOptions{
+		CancelOnError: true,
+	}
+
+	start := time.Now()
+	err := RunConcurrent(requests, opts, func(c context.Context, r *MyConcurrencyRequest, i int) (*MyConcurrencyRequest, error) {
+		r.index = i
+		r.result = r.params + i
+
+		time.Sleep(1 * time.Second)
+
+		if i%2 == 0 {
+			return r, errors.New(fmt.Sprintf("error: %d", i))
+		}
+
+		return r, nil
+	})
+	assert.Error(err)
+	assert.Equal("error: 0\nerror: 2", err.Error())
+
+	elapsed := time.Since(start)
+	assert.LessOrEqual(elapsed.Seconds(), 1.5)
 
 	sort.Slice(requests, func(i, j int) bool {
 		return requests[i].index < requests[j].index
