5a8db6a4f02219c9426eb6108510da5019267811
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2023-11-23 15:42:00 +0900

    add compare

--- compare.go
+++ compare.go
@@ -1,16 +1,82 @@
 package isunippets
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/google/go-cmp/cmp"
 )
 
 func Compare(lhs, rhs interface{}) error {
-	opts := cmp.AllowUnexported(lhs, rhs)
-	result := cmp.Diff(lhs, rhs, opts)
+	result := cmp.Diff(lhs, rhs)
 	if result != "" {
 		return errors.New(fmt.Sprintf("has diff: lhs=%p, rhs=%p, diff=%s", lhs, rhs, result))
+	} else {
+		return nil
+	}
+}
+
+func CompareJson(lhs, rhs interface{}) error {
+	if lhs == nil && rhs == nil {
+		return nil
+	} else if lhs == nil {
+		return errors.New(fmt.Sprintf("lhs is nil: rhs=%p", rhs))
+	} else if rhs == nil {
+		return errors.New(fmt.Sprintf("rhs is nil: lhs=%p", lhs))
+	}
+
+	var lhsBytes, rhsBytes []byte
+
+	switch v := lhs.(type) {
+	case []byte:
+		lhsBytes = v
+		if !json.Valid(lhsBytes) {
+			return errors.New(fmt.Sprintf("invalid lhs bytes: %v", lhsBytes))
+		}
+	case string:
+		lhsBytes = []byte(v)
+		if !json.Valid(lhsBytes) {
+			return errors.New(fmt.Sprintf("invalid lhs string: %v", lhsBytes))
+		}
+	default:
+		bs, err := json.Marshal(v)
+		if err != nil {
+			return errors.New(fmt.Sprintf("failed to marshal lhs: %v", err))
+		}
+		lhsBytes = bs
+	}
+
+	switch v := rhs.(type) {
+	case []byte:
+		rhsBytes = v
+		if !json.Valid(rhsBytes) {
+			return errors.New(fmt.Sprintf("invalid rhs bytes: %v", rhsBytes))
+		}
+	case string:
+		rhsBytes = []byte(v)
+		if !json.Valid(rhsBytes) {
+			return errors.New(fmt.Sprintf("invalid rhs string: %v", rhsBytes))
+		}
+	default:
+		bs, err := json.Marshal(v)
+		if err != nil {
+			return errors.New(fmt.Sprintf("failed to marshal rhs: %v", err))
+		}
+		rhsBytes = bs
+	}
+
+	transformJSON := cmp.FilterValues(func(x, y []byte) bool {
+		return json.Valid(x) && json.Valid(y)
+	}, cmp.Transformer("ParseJSON", func(in []byte) (out interface{}) {
+		if err := json.Unmarshal(in, &out); err != nil {
+			panic(err)
+		}
+		return out
+	}))
+
+	result := cmp.Diff(lhsBytes, rhsBytes, transformJSON)
+	if result != "" {
+		return errors.New(fmt.Sprintf("has diff json: lhs=%p, rhs=%p, diff=%s", lhs, rhs, result))
 	} else {
 		return nil
 	}
--- compare_test.go
+++ compare_test.go
@@ -1,6 +1,7 @@
 package isunippets
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
@@ -67,6 +68,98 @@ func TestCompare(t *testing.T) {
 	for i, tt := range cases {
 		t.Run(fmt.Sprintf("case %d", i), func(t *testing.T) {
 			err := Compare(tt.lhs, tt.rhs)
+			if tt.expected {
+				assert.NoError(err)
+			} else {
+				assert.Error(err)
+			}
+		})
+	}
+}
+
+func TestCompareJson(t *testing.T) {
+	assert := assert.New(t)
+
+	type MyStruct struct {
+		String   string            `json:"string"`
+		Int      int               `json:"int"`
+		Bool     bool              `json:"bool"`
+		Slice    []string          `json:"slice"`
+		Map      map[string]string `json:"map"`
+		MyStruct *MyStruct         `json:"my_struct"`
+	}
+
+	myStruct := MyStruct{
+		String: "string",
+		Int:    1,
+		Bool:   true,
+		Slice:  []string{"slice1", "slice2"},
+		Map: map[string]string{
+			"key1": "value1",
+		},
+		MyStruct: &MyStruct{
+			String: "stringInner",
+			Int:    2,
+			Bool:   false,
+			Slice:  []string{"sliceInner1", "sliceInner2"},
+			Map: map[string]string{
+				"keyInner1": "valueInner1",
+			},
+			MyStruct: nil,
+		},
+	}
+	marshalledMyStruct, err := json.Marshal(myStruct)
+	assert.NoError(err)
+
+	differentMyStruct := MyStruct{
+		String: "stringDiff",
+		Int:    1,
+		Bool:   true,
+		Slice:  []string{"slice1", "slice2"},
+		Map: map[string]string{
+			"key1": "value1Diff",
+		},
+		MyStruct: &MyStruct{
+			String: "stringInnerDiff",
+			Int:    2,
+			Bool:   false,
+			Slice:  []string{"sliceInner1", "sliceInner2"},
+			Map: map[string]string{
+				"keyInner1": "valueInner1Diff",
+			},
+			MyStruct: nil,
+		},
+	}
+	marshalledDifferentMyStruct, err := json.Marshal(differentMyStruct)
+	assert.NoError(err)
+
+	cases := []struct {
+		lhs      interface{}
+		rhs      interface{}
+		expected bool
+	}{
+		// 基本型
+		{lhs: nil, rhs: nil, expected: true},
+		{lhs: 1, rhs: 1, expected: true},
+		{lhs: "1", rhs: "1", expected: true},
+		{lhs: "\"\"", rhs: "\"\"", expected: true},
+		{lhs: "", rhs: "", expected: false},
+		{lhs: nil, rhs: "", expected: false},
+		{lhs: "", rhs: nil, expected: false},
+
+		{lhs: myStruct, rhs: myStruct, expected: true},
+		{lhs: myStruct, rhs: string(marshalledMyStruct), expected: true},
+		{lhs: myStruct, rhs: []byte(string(marshalledMyStruct)), expected: true},
+
+		{lhs: myStruct, rhs: nil, expected: false},
+		{lhs: myStruct, rhs: differentMyStruct, expected: false},
+		{lhs: myStruct, rhs: string(marshalledDifferentMyStruct), expected: false},
+		{lhs: myStruct, rhs: []byte(string(marshalledDifferentMyStruct)), expected: false},
+	}
+
+	for i, tt := range cases {
+		t.Run(fmt.Sprintf("case %d", i), func(t *testing.T) {
+			err := CompareJson(tt.lhs, tt.rhs)
 			if tt.expected {
 				assert.NoError(err)
 			} else {
